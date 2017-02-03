51346595b49506c0f97e805a551c878ba411670a
Whyhow314 <whyhow3.141@gmail.com>
2017-02-02 18:51:03 -0800

    minor edits

--- SPR.py
+++ SPR.py
@@ -36,6 +36,7 @@ class ScoutPrecision(object):
 			'gearsPlacedByLiftTele',
 			'gearsPlacedByLiftAuto'
 		]
+		
 		self.gradingListsOfDicts = [
 			'highShotTimesForBoilerTele',
 			'highShotTimesForBoilerAuto',
