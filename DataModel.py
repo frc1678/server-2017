45d355cfd0d2301852aa43f971093d2bd9b2b474
Whyhow314 <whyhow3.141@gmail.com>
2017-02-15 19:48:37 -0800

    more changes to prep for testing

--- DataModel.py
+++ DataModel.py
@@ -19,7 +19,7 @@ class Competition(object):
 		self.TIMDs = utils.makeTIMDsFromDicts(firebaseCommunicator.getPythonObjectForFirebaseDataAtLocation("/TeamInMatchDatas"))
 
 	def updateCurrentMatchNum(self):
-		self.currentMatchNum = firebaseCommunicator.getPythonObjectForFirebaseDataAtLocation("/currentMatchNumber")
+		self.currentMatchNum = firebaseCommunicator.getPythonObjectForFirebaseDataAtLocation("/currentMatchNum")
 
 class CalculatedTeamData(object):
 	"""c"""
