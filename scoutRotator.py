912c99528a44f5617e961acd11f48e4865baef15
Whyhow314 <whyhow3.141@gmail.com>
2017-02-18 16:31:21 -0800

    more cleanup

--- scoutRotator.py
+++ scoutRotator.py
@@ -1,7 +1,6 @@
 import pyrebase
 import DataModel
 import SPR
-import multiprocessing
 
 config = {
 	"apiKey": "mykey",
@@ -12,9 +11,10 @@ config = {
 
 f = pyrebase.initialize_app(config)
 fb = f.database()
-testScouts = "scout1 scout10 scout11 scout12 scout13 scout14 scout15 scout16 scout17 scout18 scout2 scout3".split()
+testScouts = "scout1 scout10 scout11 scout12 scout2 scout3 scout4 scout5 scout6 scout7 scout8 scout9".split()
 scouts = "janet justin alex wesley kyle mx aiden westley katie jesse jack sage jon ayush sam evan mingyo zoe gemma carter".split()
 SPR = SPR.ScoutPrecision()
+
 #creates list of availability values in firebase for each scout
 def resetAvailability():
 	availability = {name: 1 for name in testScouts}
@@ -23,7 +23,8 @@ def resetAvailability():
 
 #creates firebase objects for 18 scouts
 def resetScouts():
-	scouts = {'scout' + str(num) : {'currentUser': ''} for num in range(1,13)}
+	scouts = {'scout' + str(num) : {'currentUser': '', 'scoutStatus': ''} for num in range(1,13)}
+																					#change 13 to 19 for actual use
 	fb.child('scouts').set(scouts)
 
 def doSPRsAndAssignments(newMatchNumber):
@@ -37,7 +38,7 @@ def doSPRsAndAssignments(newMatchNumber):
 	available = [k for (k, v) in fb.child("availability").get().val().items() if v == 1]
 	#Each scout is assigned to a robot in the next 2 lines
 	SPR.calculateScoutPrecisionScores(fb.child("TempTeamInMatchDatas").get().val(), available)
-	# print SPR.sprs
+	print SPR.sprs
 	newAssignments = SPR.assignScoutsToRobots(available, redTeams + blueTeams, fb.child("scouts").get().val())
 	#and it is put on firebase
 	fb.child("scouts").update(newAssignments)
