ca77f29ffb2a6dd1cda5ebf17d927ea1ae893bd6
Whyhow314 <whyhow3.141@gmail.com>
2017-02-22 20:33:36 -0800

    testing stuff

--- scoutRotator.py
+++ scoutRotator.py
@@ -11,7 +11,7 @@ config = {
 
 f = pyrebase.initialize_app(config)
 fb = f.database()
-testScouts = "a b c d e f g h i".split()
+testScouts = "a b c d e f g h i j k l".split()
 scouts = "janet justin alex wesley kyle mx aiden westley katie jesse jack sage jon ayush sam evan mingyo zoe gemma carter".split()
 SPR = SPR.ScoutPrecision()
 
@@ -23,8 +23,8 @@ def resetAvailability():
 
 #creates firebase objects for 18 scouts
 def resetScouts():
-	scouts = {'scout' + str(num) : {'currentUser': '', 'scoutStatus': ''} for num in range(1,10)}
-																					#change 10 to 19 for actual use
+	scouts = {'scout' + str(num) : {'currentUser': '', 'scoutStatus': ''} for num in range(1,13)}
+																					#change 13 to 19 for actual use
 	fb.child('scouts').set(scouts)
 
 def doSPRsAndAssignments(newMatchNumber):
