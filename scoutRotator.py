31fdb1d4e94bbdb00dd5ac4c2db4ee3ca903a350
Whyhow314 <whyhow3.141@gmail.com>
2017-03-05 15:25:01 -0800

    fixed already assigned stream

--- scoutRotator.py
+++ scoutRotator.py
@@ -1,6 +1,6 @@
 import pyrebase
 import DataModel
-import spr
+import SPR
 import firebaseCommunicator
 
 PBC = firebaseCommunicator.PyrebaseCommunicator()
@@ -8,7 +8,8 @@ PBC.initializeFirebase()
 fb = PBC.firebase
 #testScouts = "calvin ethan nathan wentao janet carter kenny ryan nate astha astha gemma livy ben".split()
 scouts = "janet justin alex wesley kyle mx aiden westley katie jesse jack sage jon ayush sam evan mingyo zoe gemma carter".split()
-spr = spr.ScoutPrecision()
+SPR = SPR.ScoutPrecision()
+oldMatchNum = 0
 
 #Creates list of availability values in firebase for each scout
 def resetAvailability():
@@ -46,14 +47,13 @@ def tabletHandoutStream():
 
 #Use this for running the server again (e.g. after a crash) to avoid reassigning scouts
 def alreadyAssignedStream():
-	startMatchNum = fb.child("currentMatchNum").get().val()
-	newMatchNum = startMatchNum + 1
-	fb.child("currentMatchNum").stream(lambda d: startStreamAfterAssignment(d, newMatchNum))
+	global oldMatchNum
+	oldMatchNum = fb.child("currentMatchNum").get().val()
+	fb.child("currentMatchNum").stream(startAtNewMatch)
 
-def startStreamAfterAssignment(newNum, newerNum):
-	if newNum.get("data") == None: return
-	if newNum["data"] == newerNum:
-		dosprsAndAssignments(newNum)
+def startAtNewMatch(newMatchNum):
+	if fb.child("currentMatchNum").get().val() > oldMatchNum:
+		dosprsAndAssignments(newMatchNum)
 
 #Use this if you are restarting the server and need to reassign scouts but scouts already have tablets
 #Also useful for unexpected changes in availability
