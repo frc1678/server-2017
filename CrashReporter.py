5c720735494aad1aac77c66c4ecf02a7299b01dc
Kenny Zhou <kezzhou@gmail.com>
2017-03-18 14:06:31 -0700

    Space

--- CrashReporter.py
+++ CrashReporter.py
@@ -3,6 +3,7 @@ import threading
 from slackclient import SlackClient
 import time
 import pdb
+
 def reportServerCrash(message):
 	sc = SlackClient("xoxp-49302180423-49316952386-140114605008-32492dbe60a2bb10d0f3d5cd92194ccc")
 	map(lambda u:
