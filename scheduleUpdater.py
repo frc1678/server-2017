34fecdcf684787eb133b04c89063b09eac608023
Kenny Zhou <kezzhou@gmail.com>
2017-04-28 15:17:59 -0700

    Simplified Things

--- scheduleUpdater.py
+++ scheduleUpdater.py
@@ -4,19 +4,18 @@ import sys
 
 PBC = firebaseCommunicator.PyrebaseCommunicator()
 fb = PBC.firebase
+
 def update(data):
-	print (data)
+	print(data)
 	if data['data'] == None:
 		latest = 1
+	elif data['data'] == 1:
+		keys = map(lambda k: int(k.split('Q')[1].split('-')[0]), fb.child('TempTeamInMatchDatas').shallow().get().each())
+		latest = sorted(keys, reverse = True)[0] + 1
+		fb.child('matchFinished').set(0)
 	else:
-		if data['data'] == 1:
-			keys = map(lambda k: int(k.split('Q')[1].split('-')[0]), fb.child("TempTeamInMatchDatas").shallow().get().each())
-			latest = sorted(keys, reverse=True)[0] + 1
-			fb.child('matchFinished').set(0)
-		else:
-			return
 		fb.child('currentMatchNum').set(latest)
+		return
 
 def scheduleListener():
 	fb.child('matchFinished').stream(update)
-
