f1db64a2061f0f5fc9978f78b3fa14da08ca8ff6
Kenny Zhou <kezzhou@gmail.com>
2017-05-05 12:05:55 -0700

    comments

--- scheduleUpdater.py
+++ scheduleUpdater.py
@@ -7,15 +7,19 @@ fb = PBC.firebase
 
 def update(data):
 	print(data)
+	#prints and updates schedule on firebase based on the data
 	if data['data'] == None:
 		latest = 1
 	elif data['data'] == 1:
 		keys = map(lambda k: int(k.split('Q')[1].split('-')[0]), fb.child('TempTeamInMatchDatas').shallow().get().each())
+		#makes a list of keys by sorting through every TempTIMD on firebase
 		latest = sorted(keys, reverse = True)[0] + 1
+		#sorts the keys to find the latest
 		fb.child('matchFinished').set(0)
 	else:
 		fb.child('currentMatchNum').set(latest)
-		return
+		#sets currentMatchNum on firebase
 
 def scheduleListener():
+	#call after update(), starts an update stream on firebase
 	fb.child('matchFinished').stream(update)
