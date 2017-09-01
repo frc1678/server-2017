ef7b537e0debe0f633e50dabcb3a947530b4a0bc
Kenny Zhou <kezzhou@gmail.com>
2017-08-31 19:54:25 -0700

    deleted unecessary modules from crashReporter
    also cleaned some stuff up in crashReporter

--- CrashReporter.py
+++ CrashReporter.py
@@ -1,26 +1,22 @@
-#Last Updated: 8/26/17
-import smtplib
-import threading
+#Last Updated: 8/31/17
 from slackclient import SlackClient
-import time
-import pdb
 
 sc = SlackClient('Slack Api')
 
-#Sends slack message to listed users
+#Sends slack message to listed user(s)
 def reportServerCrash(message):
 	map(lambda u:
 	sc.api_call(
 		'chat.postMessage',
 		channel = '@' + u,
 		text = message
-	), [''])
+	), ['users'])
 
-#Sends slack message to listed user
+#Sends slack message to listed user(s)
 def reportOverestimate(message):
 	map(lambda u:
 	sc.api_call(
 		'chat.postMessage',
 		channel = '@' + u,
 		text = message
-	), [''])
+	), ['users'])
