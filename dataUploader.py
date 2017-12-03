d56dcbf3bf1e7335eaf7e64cb901ddea12675877
Kenny Zhou <kezzhou@gmail.com>
2017-12-03 14:58:19 -0800

    Template for dataUploader testing data stuff

--- dataUploader.py
+++ dataUploader.py
@@ -1,4 +1,4 @@
-#Last Updated: 11/11/17
+#Last Updated: 12/3/17
 import pyrebase
 import random
 import time
@@ -12,15 +12,7 @@ class CalculatedTeamInMatchData(object):
 		#initializer for random CalculatedTIMDs
 		super(CalculatedTeamInMatchData, self).__init__()
 		self.numRPs = random.randint(0, 4)
-		self.liftoffAbility = random.randint(0, 50)
-		self.numHighShotsTele = random.randint(0, 50)
-		self.numHighShotsAuto = random.randint(0, 50)
-		self.numLowShotsTele = random.randint(0, 50)
-		self.numLowShotsAuto = random.randint(0, 50)
-		self.numGearsPlacedTele = random.randint(0, 50)
-		self.numGearsPlacedAuto = random.randint(0, 50)
-		self.wasDisfunctional = bool(random.randint(0, 1))
-		self.avgKeyShotTime = random.randint(0, 50)
+		#main calculatedData points, random int between lowest possible (0) and mediocre-high points.
 
 class TeamInMatchData(object):
 	'''An FRC TeamInMatchData Object'''
@@ -33,61 +25,16 @@ class TeamInMatchData(object):
 		 self.matchNumber = args['matchNumber']
 		 self.scoutName = args['scoutName']
 		 self.superNotes = random.randint(0, 1)
-		 self.numHoppersOpenedTele = random.randint(0, 1)
-		 self.numGearGroundIntakesTele = random.randint(0, 1)
-		 self.numGearLoaderIntakesTele = random.randint(0, 1)
-		 self.numGearsEjectedTele = random.randint(0, 1)
-		 self.numGearsFumbledTele = random.randint(0, 1)
-		 self.didReachBaselineAuto = random.randint(0, 1)
-		 self.numHoppersOpenedAuto = random.randint(0, 1)
-		 self.didLiftoff = bool(random.randint(0, 1))
+		#main, more 'random' auto and tele parts, for 2017: hoppers opened/used, didLiftOff, gears ejected, fumbles, gear and fuel intake
 		 self.didStartDisabled = bool(random.randint(0, 1))
 		 self.didBecomeIncapacitated = bool(random.randint(0, 1))
 		'''
 		self.rankSpeed = random.randint(0, 4)
 		self.rankAgility = random.randint(0, 4)
-		self.rankGearControl = random.randint(0, 4)
-		self.rankBallControl = random.randint(0, 4)
+		#main controls, super stuff, for 2017: ball control and gear control
 		self.rankDefense = random.randint(0, 4)
 		'''
-		 self.gearsPlacedByLiftAuto = {
-		 	'lift1' : random.randint(0, 1),
-		 	'lift2' : random.randint(0, 1),
-		 	'lift3' : random.randint(0, 1)
-		 }
-		 self.gearsPlacedByLiftTele = {
-		 	'lift1' : random.randint(0, 1),
-		 	'lift2' : random.randint(0, 1),
-		 	'lift3' : random.randint(0, 1)
-		 }
-		 self.highShotTimesForBoilerAuto = [
-		 	{
-		 		'time' : random.randint(0, 1),
-		 		'numShots' : random.randint(0, 1),
-		 		'position' : 'Hopper'
-		 	}
-		 ]
-		 self.lowShotTimesForBoilerAuto = [
-		 	{
-		 		'time' : random.randint(0, 1),
-		 		'numShots' : random.randint(0, 1),
-		 		'position' : 'Hopper'
-		 	}
-		 ]
-		 self.highShotTimesForBoilerTele = [
-		 	{
-		 		'time' : random.randint(0, 1),
-		 		'numShots' : random.randint(0, 1),
-		 		'position' : 'Key'
-		 	}
-		 ]
-		 self.lowShotTimesForBoilerTele = [
-		 	{
-		 		'time' : random.randint(0, 1),
-		 		'numShots' : random.randint(0, 1),
-		 		'position' : 'Key'
-		 	}
-		 ]
+		 Other information needed, for 2017: gears placed, where placed, auto and tele, highShotTimesAuto, highShotTimesTele, and same with low shot
 		'''
 
 class Match(object):
@@ -99,13 +46,7 @@ class Match(object):
 		self.calculatedData = None
 		self.redAllianceTeamNumbers = args['redAllianceTeamNumbers']
 		self.blueAllianceTeamNumbers = args['blueAllianceTeamNumbers']
-		self.numRotorsSpinningRedAuto = random.randint(0, 1)
-		self.numRotorsSpinningRedTele = random.randint(0, 1)
-		self.numRotorsSpinningBlueAuto = random.randint(0, 1)
-		self.numRotorsSpinningBlueTele = random.randint(0, 1)
-		self.blueDidStartAllRotors = random.randint(0, 1)
-		self.redDidReachFortyKilopascals = random.randint(0, 1)
-		self.blueDidReachFortyKilopascals = random.randint(0, 1)
+		#match data, mostly presented after each match on the screen.
 		self.redScore = random.randint(0, 1)
 		self.blueScore = random.randint(0, 1)
 		self.foulPointsGainedRed = random.randint(0, 1)
@@ -126,10 +67,7 @@ class CalculatedMatchData(object):
 		self.actualBlueRPs = random.random() * 4
 		self.predictedRedRPs = random.random() * 4
 		self.actualRedRPs =  random.random() * 4
-		self.fortyKilopascalChanceRed = random.random()
-		self.fortyKilopascalChanceBlue = random.random()
-		self.allRotorsTurningChanceRed = random.random()
-		self.allRotorsTurningChanceBlue = random.random()
+		#biggest ways of earning points, 2017: forty Kilopascals Chance for each alliance, and 4 rotors turned chance for each alliance
 		self.__dict__.update(args)
 
 config = {
